0dcd22abe6deb79b2d1f9ec90813699f1911bbd2
Efim <dolgoff.efim@yandex.com>
2022-10-29 18:52:58 +0300

    Version 1.1

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import vk_api
 session = vk_api.VkApi(token='4241c8084241c8084241c808e44150f6ca442414241c808211dea98b9fb21e3557ebf6c')
 vk1 = session.get_api()
 
-def get_user_friends(user_id):
-    friends = session.method("friends.get", {"user_id": user_id})
+def get_user_friends(user_id, fields):
+    friends = session.method("friends.get", {"user_id": user_id, "fields": fields})
     print(friends)
     
-get_user_friends(96318884)
+get_user_friends(int(input()), "nickname")
