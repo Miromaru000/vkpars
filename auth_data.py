1f34ca0f0b0f30721e427b231318c4b417ffaa46
Efim <dolgoff.efim@yandex.com>
2022-10-30 11:40:47 +0300

    Version 2.0.1

--- auth_data.py
+++ auth_data.py
@@ -1,19 +1,13 @@
-from urllib import response
 import requests
 
 rec = requests.get('https://oauth.vk.com/access_token?client_id=51461826&client_secret=OQxRqwIiV2muJFJCXYCe&redirect_uri=http://127.0.0.1&code=13b67e52e0d225f55b')
-
-
 rec = rec.json()
 
 token = rec.get('access_token')
 
 
 
-
-#def get_access_token(clinet_id, client_secret, redirect_uri, code):
   
-#token = get_access_token(51461826,'OQxRqwIiV2muJFJCXYCe', 'http://127.0.0.1' ,input())   
    
 #token = '012f6b0b012f6b0b012f6b0bcd023e55c90012f012f6b0b627238b663c0a75980e960d8'
 
